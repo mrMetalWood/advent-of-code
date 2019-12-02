1f15e2bd37503cb4980122e70d9d0ff72225e6b7
Malte Modrow <malte.modrow@gmail.com>
2019-12-02 09:22:35 +0100

    improve by looking at others code :D

--- 2019/day-02/day-02.py
+++ 2019/day-02/day-02.py
@@ -4,25 +4,22 @@ with open('2019/day-02/input.txt', 'r') as file:
 
 def run_intcode_program(noun, verb):
     memory = numbers.copy()
-    instruction_pointer = 0
+    pointer = 0
 
     memory[1] = noun
     memory[2] = verb
 
     while True:
-        opcode = memory[instruction_pointer]
-        address = memory[instruction_pointer + 3]
-        param1 = memory[memory[instruction_pointer + 1]]
-        param2 = memory[memory[instruction_pointer + 2]]
+        opcode, param1, param2, address = memory[pointer: pointer + 4]
 
         if opcode == 1:
-            memory[address] = param1 + param2
+            memory[address] = memory[param1] + memory[param2]
         elif opcode == 2:
-            memory[address] = param1 * param2
+            memory[address] = memory[param1] * memory[param2]
         elif opcode == 99:
             break
 
-        instruction_pointer += 4
+        pointer += 4
 
     return memory[0]
 
@@ -34,8 +31,7 @@ def part1():
 def part2():
     for noun in range(0, 100):
         for verb in range(0, 100):
-            result = run_intcode_program(noun, verb)
-            if result == 19690720:
+            if run_intcode_program(noun, verb) == 19690720:
                 return 100 * noun + verb
 
 
