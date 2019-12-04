9a4e8f0135089c9f02addfb7fcbedc758a2c0613
Malte Modrow <malte.modrow@gmail.com>
2019-12-04 12:34:59 +0100

    inspiration by others

--- 2019/day-04/day-04.py
+++ 2019/day-04/day-04.py
@@ -1,3 +1,5 @@
+import itertools
+
 password_min = 168630
 password_max = 718098
 
@@ -7,25 +9,16 @@ def parse_password_range():
 
     for possible_password in range(password_min, password_max + 1):
         decreased = double = standalone_double = False
-        numbers = [int(n) for n in str(possible_password)]
-        count = 1
+        last_digit = 0
 
-        for index, current in enumerate(numbers):
-            prev_item = numbers[index - 1] if index > 0 else -99
-            next_item = numbers[index + 1] if index < len(numbers) - 1 else 99
+        for current, group in itertools.groupby(list(str(possible_password)), lambda x: x):
+            number_count = len(list(group))
 
-            decreased = True if prev_item > current else decreased
-            double = True if prev_item == current else double
+            double = True if number_count >= 2 else double
+            standalone_double = True if number_count == 2 else standalone_double
+            decreased = True if int(current) < last_digit else decreased
 
-            new_count = count + 1 if prev_item == current else 1
-
-            is_ex_double = count == 2 and new_count == 1
-            is_ex_double_at_end = index == len(numbers) - 1 and new_count == 2
-
-            if is_ex_double or is_ex_double_at_end:
-                standalone_double = True
-
-            count = new_count
+            last_digit = int(current)
 
         passwords.setdefault(
             possible_password, (decreased, double, standalone_double)
