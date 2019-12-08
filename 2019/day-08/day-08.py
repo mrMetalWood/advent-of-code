7993e4926b2378131fa2317d1456dc22adab2905
Malte Modrow <malte.modrow@gmail.com>
2019-12-08 15:11:46 +0100

    improve

--- 2019/day-08/day-08.py
+++ 2019/day-08/day-08.py
@@ -2,22 +2,18 @@ import textwrap
 
 with open('2019/day-08/input.txt', 'r') as file:
     pixel = list(map(int, file.read()))
-
-
-def get_layers(width, height):
-    step = width * height
-    return [pixel[i:i + step] for i in range(0, len(pixel), step)]
+    step = 25 * 6
+    layers = [pixel[i:i + step] for i in range(0, len(pixel), step)]
 
 
 def part1():
-    layers = get_layers(25, 6)
     fewest_0 = sorted(layers, key=lambda layer: layer.count(0))[0]
     return fewest_0.count(1) * fewest_0.count(2)
 
 
 def part2():
     final_image = []
-    for pixels in zip(*get_layers(25, 6)):
+    for pixels in zip(*layers):
         final_image.append(
             next("◼" if p == 1 else '_' for p in pixels if p == 0 or p == 1))
     return ''.join(final_image)
