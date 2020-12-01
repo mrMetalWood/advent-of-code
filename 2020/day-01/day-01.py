e04fb278fd7048e2bc94e282ab2ffaa09e77ebe5
Malte Modrow <modrow@ubilabs.net>
2020-12-01 08:16:36 +0100

    tweak

--- 2020/day-01/day-01.py
+++ 2020/day-01/day-01.py
@@ -1,17 +1,14 @@
 import os
+import itertools
 
-input_file = os.path.join(os.path.dirname(__file__), "input.txt")
-
-with open(input_file, "r") as file:
+with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as file:
     numbers = [int(l.strip()) for l in file.readlines()]
 
-for n in numbers:
-    for m in numbers:
-        if n + m == 2020:
-            part1 = n * m
-        for l in numbers:
-            if n + m + l == 2020:
-                part2 = n * m * l
+for i, j, k in itertools.combinations(numbers, 3):
+    if i + j == 2020:
+        part1 = i * j
+    if i + j + k == 2020:
+        part2 = i * j * k
 
 
 print(f"Part 1: {part1}")  # 1010299
