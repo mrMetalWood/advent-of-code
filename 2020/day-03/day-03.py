74f3f5aaba03856a3a2f7989b85f6d19dd99fc64
Malte Modrow <modrow@ubilabs.net>
2020-12-03 11:01:04 +0100

    tweak

--- 2020/day-03/day-03.py
+++ 2020/day-03/day-03.py
@@ -5,21 +5,19 @@ with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as file:
     grid = [l.strip() for l in file.readlines()]
 
 
-def count_trees(right, down):
-    col = trees = 0
+def trees(right, down):
+    col = count = 0
 
     for row in range(0, len(grid), down):
         if grid[row][col] == "#":
-            trees += 1
+            count += 1
         col = (col + right) % len(grid[0])
 
-    return trees
+    return count
 
 
-part1 = count_trees(3, 1)
-part2 = math.prod(
-    [count_trees(r, d) for r, d in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]]
-)
+part1 = trees(3, 1)
+part2 = math.prod([trees(r, d) for r, d in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]])
 
 print(f"Part 1: {part1}")  # 250
 print(f"Part 2: {part2}")  # 1592662500
