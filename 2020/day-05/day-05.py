b8e077acc5e53d4bc0d0eacb6c2fefba899124e2
Malte Modrow <modrow@ubilabs.net>
2020-12-05 10:06:28 +0100

    golfing

--- 2020/day-05/day-05.py
+++ 2020/day-05/day-05.py
@@ -1,18 +1,14 @@
 import os
+import bisect
 
 with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as file:
     passes = [l.strip() for l in file.readlines()]
 
-bin_map = {"F": "0", "B": "1", "L": "0", "R": "1"}
-ids = []
+ids, bin_map = [], {"F": "0", "B": "1", "L": "0", "R": "1"}
 for p in passes:
     row = int("".join([bin_map[i] for i in p[:7]]), 2)
     col = int("".join([bin_map[i] for i in p[-3:]]), 2)
-    ids.append(row * 8 + col)
-ids = sorted(ids)
+    bisect.insort(ids, row * 8 + col)
 
-part1 = ids[-1]
-part2 = [id for id in range(ids[0], ids[-1] + 1) if id not in ids][0]
-
-print(f"Part 1: {part1}")  # 935
-print(f"Part 2: {part2}")  # 743
+print(f"Part 1: {ids[-1]}")  # 935
+print(f"Part 2: {[id for id in range(ids[0], ids[-1] + 1) if id not in ids][0]}")  # 743
