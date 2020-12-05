0f6a4c5a395eb4f0c58db6f3f46d4a747c415a5e
Malte Modrow <modrow@ubilabs.net>
2020-12-05 11:22:45 +0100

    golfing

--- 2020/day-05/day-05.py
+++ 2020/day-05/day-05.py
@@ -1,7 +1,7 @@
 with open("2020/day-05/input.txt", "r") as file:
     ids = sorted([
-        int("".join([str(int(i in ["B", "R"])) for i in p]), 2)
-        for p in [l.strip() for l in file.readlines()]
+        int("".join([str(int(p in ["B", "R"])) for p in passes]), 2)
+        for passes in [line.strip() for line in file.readlines()]
     ])
     print(f"Part 1: {ids[-1]}")  # 935
     print(f"Part 2: {[id for id in range(ids[0], ids[-1] + 1) if id not in ids][0]}")  # 743
