cc74697563ed2a976e2f0b63a2f5ec8605f888f3
Malte Modrow <modrow@ubilabs.net>
2020-12-07 08:37:51 +0100

    day-07

---
+++ 2020/day-07/day-07.py
@@ -0,0 +1,42 @@
+from collections import deque
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as file:
+    rules = [l.strip() for l in file.readlines()]
+    colors = {}
+    for rule in rules:
+        color, contents = rule.split(" bags contain ")
+        contents = [
+            (" ".join(i.split()[1:3]), int(i.split()[0]))
+            for i in contents.split(",")
+            if i.split()[0].isnumeric()
+        ]
+        colors[color] = contents
+
+
+def part1():
+    color_queue, color_with_shiny_gold = deque(["shiny gold"]), set()
+
+    while color_queue:
+        current_color = color_queue.popleft()
+        for color in colors:
+            for (color_name, _) in colors[color]:
+                if color_name == current_color:
+                    color_queue.append(color)
+                    color_with_shiny_gold.add(color)
+
+    return len(color_with_shiny_gold)
+
+
+def part2():
+    def count_bags(color):
+        count = 1
+        for (bag_color, amount) in colors[color]:
+            count += count_bags(bag_color) * amount
+        return count
+
+    return count_bags("shiny gold") - 1
+
+
+print(f"Part 1: {part1()}")  # 139
+print(f"Part 2: {part2()}")  # 58175
