3438cbd6396d746b78ca160e1b5b4109e7f8a21d
Malte Modrow <modrow@ubilabs.net>
2020-12-10 09:04:24 +0100

    tweak

--- 2020/day-10/day-10.py
+++ 2020/day-10/day-10.py
@@ -2,22 +2,22 @@ import os
 from collections import Counter
 
 with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as file:
-    numbers = sorted([int(l.strip()) for l in file.readlines()])
-    numbers = [0] + numbers + [numbers[-1] + 3]
+    adapters = sorted([int(l.strip()) for l in file.readlines()])
+    adapters = [0] + adapters + [adapters[-1] + 3]
 
 
 def part1():
-    diffs = Counter([high - low for low, high in zip(numbers, numbers[1:])])
+    diffs = Counter([high - low for low, high in zip(adapters, adapters[1:])])
     return diffs[1] * diffs[3]
 
 
 def part2():
-    counts = [0] * len(numbers)
+    counts = [0] * len(adapters)
     counts[0] = 1
 
-    for i in range(1, len(numbers)):
+    for i in range(1, len(adapters)):
         for j in range(i):
-            if numbers[i] - numbers[j] <= 3:
+            if adapters[i] - adapters[j] <= 3:
                 counts[i] += counts[j]
 
     return counts[-1]
