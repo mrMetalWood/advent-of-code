459f72e35309d60b316090c946e42235fbcc94f0
Malte Modrow <modrow@ubilabs.net>
2020-12-15 10:34:19 +0100

    tweak

--- 2020/day-15/day-15.py
+++ 2020/day-15/day-15.py
@@ -1,21 +1,8 @@
-from collections import defaultdict
-import os
+ledger = {n: idx + 1 for idx, n in enumerate([1, 17, 0, 10, 18, 11, 6])}
+spoken_number = list(ledger)[-1]
 
-
-def solve(spoken_number_count):
-    ledger = defaultdict(lambda: [])
-    for idx, n in enumerate([1, 17, 0, 10, 18, 11, 6]):
-        ledger[int(n)].append(idx + 1)
-    spoken_number = list(ledger.keys())[-1]
-
-    for i in range(len(ledger) + 1, spoken_number_count + 1):
-        if len(ledger[spoken_number]) >= 2:
-            spoken_number = ledger[spoken_number][-1] - ledger[spoken_number][-2]
-        else:
-            spoken_number = 0
-        ledger[spoken_number].append(i)
-    return spoken_number
-
-
-print(f"Part 1: {solve(2020)}")  # 595
-print(f"Part 2: {solve(30000000)}")  # 1708310
+for turn in range(len(ledger), 30000000):
+    ledger[spoken_number], spoken_number = turn, turn - ledger.get(spoken_number, turn)
+    if turn == 2020 - 1:
+        print(f"Part 1: {spoken_number}")  # 595
+print(f"Part 2: {spoken_number}")  # 1708310
