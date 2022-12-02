36f048b11280c1c746edc1bb2218812ff5f0b639
Malte Modrow <modrow@ubilabs.net>
2022-12-02 10:14:04 +0100

    tweak

--- 2022/day-02/day-02.py
+++ 2022/day-02/day-02.py
@@ -4,33 +4,21 @@ with open(os.path.join(os.path.dirname(__file__), "input.txt"), "r") as file:
     plays = [l.strip() for l in file.readlines()]
 
     points = {
-        "A X": 4,  # rock rock
-        "A Y": 8,  # rock paper
-        "A Z": 3,  # rock scissors
-        "B X": 1,  # paper rock
-        "B Y": 5,  # paper paper
-        "B Z": 9,  # paper scissors
-        "C X": 7,  # scissors rock
-        "C Y": 2,  # scissors paper
-        "C Z": 6,  # scissors scissors
-    }
-
-    points2 = {
-        "A X": 3,  # rock scissors
-        "A Y": 4,  # rock rock
-        "A Z": 8,  # rock paper
-        "B X": 1,  # paper rock
-        "B Y": 5,  # paper paper
-        "B Z": 9,  # paper scissors
-        "C X": 2,  # scissors paper
-        "C Y": 6,  # scissors scissors
-        "C Z": 7,  # scissors rock
+        "A X": (4, 3),  # rock rock | rock scissors
+        "A Y": (8, 4),  # rock paper | rock rock
+        "A Z": (3, 8),  # rock scissors | rock paper
+        "B X": (1, 1),  # paper rock | paper rock
+        "B Y": (5, 5),  # paper paper | paper paper
+        "B Z": (9, 9),  # paper scissors | paper scissors
+        "C X": (7, 2),  # scissors rock | scissors paper
+        "C Y": (2, 6),  # scissors paper | scissors scissors
+        "C Z": (6, 7),  # scissors scissors | scissors rock
     }
 
     score = score2 = 0
     for play in plays:
-        score += points[play]
-        score2 += points2[play]
+        score += points[play][0]
+        score2 += points[play][1]
 
     print(f"Part 1: {score}")  # 14163
     print(f"Part 2: {score2}")  # 12091
